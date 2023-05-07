6c1161fc51b3059ea55dd3c7e7ec4388875e8c0e
Fabian910 <fabianfunes915@gmail.com>
2023-05-07 20:45:33 -0300

    act

---
+++ spring/HolaMundoThymeleaf/src/main/java/arg/fabian/uno/HolaMundo/HolaMundoApplication.java
@@ -0,0 +1,16 @@
+package arg.fabian.uno.HolaMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoApplication.class, args);
+
+
+	}
+
+
+}
